f3ec7246546a0b844befe5ea34e447bc42c38341
zainabsirajo50 <zainabsirajo50@gmail.com>
2023-04-14 14:18:02 -0400

    project 7 4/14

--- app/src/main/java/com/example/randomspaceimage/MainActivity.kt
+++ app/src/main/java/com/example/randomspaceimage/MainActivity.kt
@@ -3,41 +3,91 @@ package com.example.randomspaceimage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-import com.example.randomspaceimage.R
 import okhttp3.Headers
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var spaceList: MutableList<Triple<String, String, String>>
+    private lateinit var rvSpace: RecyclerView
     var planetImageURL = ""
+    var spaceNameURL = ""
+    var spaceDateURL = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPlanetImageURL()
-        Log.d("spaceImageURL", "space image URL set")
-        val button = findViewById<Button>(R.id.spaceButton)
-        val image = findViewById<ImageView>(R.id.spaceImage)
-        getNextImage(button, image)
-    }
 
+        getPlanetImageURL()
+   //     getSpaceNameURL()
+    //  getSpaceDateURL()
+
+        rvSpace = findViewById(R.id.spaceList)
+        spaceList = mutableListOf()
+
+
+  /*      val button = findViewById<Button>(R.id.spaceButton)
+        val image = findViewById<ImageView>(R.id.spaceImage)
+        val text1 = findViewById<TextView>(R.id.spaceText1)
+        val text2 = findViewById<TextView>(R.id.spaceText2)*/
+
+
+
+
+
+        val adapter = SpaceAdapter(spaceList)
+        rvSpace.adapter = adapter
+        rvSpace.layoutManager = LinearLayoutManager(this@MainActivity)
+
+
+        Log.d("spaceImageURL", "space image URL set")
+
+
+       // getNextImage(button, image, text1, text2)
+
+
+    }
 
     private fun getPlanetImageURL() {
         val client = AsyncHttpClient()
 
-        client["https://api.nasa.gov/planetary/apod?api_key=XOwzd9OokurdcLHrvyZgurbUQKhoBNVjcPksQ6yQ&count=1", object : JsonHttpResponseHandler() {
+        client["https://api.nasa.gov/planetary/apod?api_key=XOwzd9OokurdcLHrvyZgurbUQKhoBNVjcPksQ6yQ&count=20", object : JsonHttpResponseHandler() {
             override fun onSuccess(
                 statusCode: Int,
                 headers: Headers,
                 json: JsonHttpResponseHandler.JSON
             ) {
-                Log.d("Image", "response successful$json")
-                var resultsJSON = json.jsonArray.getJSONObject(0)
-                planetImageURL = resultsJSON.getString("url")
+                Log.d("Image Photo", "response successful$spaceList")
+
+
+                var spaceImageArray = json.jsonArray.getJSONObject(0)
+
+
+
+                for (i in 0 until 20) {
+                    spaceImageArray = json.jsonArray.getJSONObject(i)
+
+
+                    planetImageURL = spaceImageArray.getString("url")
+
+                    spaceNameURL = spaceImageArray.getString("title")
+
+                    spaceDateURL = spaceImageArray.getString("date")
+
+                    spaceList.add(Triple(planetImageURL, spaceNameURL, spaceDateURL))
+                }
+
+
+
+
+                val adapter = SpaceAdapter(spaceList)
+                rvSpace.adapter = adapter
+                rvSpace.layoutManager = LinearLayoutManager(this@MainActivity)
+
+
+
             }
 
 
@@ -52,10 +102,117 @@ class MainActivity : AppCompatActivity() {
         }]
     }
 
+   /*private fun getSpaceNameURL() {
+        val client = AsyncHttpClient()
 
-    private fun getNextImage(button: Button, imageView: ImageView) {
+        client["https://api.nasa.gov/planetary/apod?api_key=XOwzd9OokurdcLHrvyZgurbUQKhoBNVjcPksQ6yQ&count=20", object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+
+
+                Log.d("Image Name", "response successful$spaceList")
+
+                var spaceTitleArray = json.jsonArray.getJSONObject(0)
+
+
+
+                for (i in 0 until spaceTitleArray.length()) {
+                    spaceTitleArray = json.jsonArray.getJSONObject(i)
+                    spaceNameURL = spaceTitleArray.getString("title")
+
+                    spaceList.add(spaceNameURL)
+
+
+                }
+
+                var spaceDateArray = json.jsonArray.getJSONObject(0)
+
+
+
+                for (i in 0 until spaceDateArray.length()) {
+                    spaceDateArray = json.jsonArray.getJSONObject(i)
+                    spaceDateURL = spaceDateArray.getString("date")
+
+                    spaceList.add(spaceDateURL)
+                }
+                Log.d("Image Date", "response successful$spaceList")
+
+                val adapter = SpaceAdapter(spaceList)
+                rvSpace.adapter = adapter
+                rvSpace.layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Image Name Error", errorResponse)
+            }
+        }]
+
+
+
+    }
+
+
+   private fun getSpaceDateURL() {
+        val client = AsyncHttpClient()
+
+        client["https://api.nasa.gov/planetary/apod?api_key=XOwzd9OokurdcLHrvyZgurbUQKhoBNVjcPksQ6yQ&count=1", object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+                Log.d("Image Description", "response successful$spaceList")
+
+
+                var spaceDateArray = json.jsonArray.getJSONObject(0)
+
+
+
+                for (i in 0 until spaceDateArray.length()) {
+                    spaceDateArray = json.jsonArray.getJSONObject(i)
+                    spaceDateURL = spaceDateArray.getString("date")
+
+                    spaceList.add(spaceDateURL)
+                }
+
+
+
+                val adapter = SpaceAdapter(spaceList)
+                rvSpace.adapter = adapter
+                rvSpace.layoutManager = LinearLayoutManager(this@MainActivity)
+
+            }
+
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Image Date Error", errorResponse)
+            }
+        }]
+    }
+*/
+
+  /*  private fun getNextImage(button: Button, imageView: ImageView, spaceName: TextView, spaceDescription: TextView) {
         button.setOnClickListener {
             getPlanetImageURL()
+            getSpaceNameURL()
+            getSpaceDateURL()
+
+            spaceName.setText(spaceNameURL)
+            spaceDescription.setText(spaceDateURL)
 
             Glide.with(this)
                 .load(planetImageURL)
@@ -63,5 +220,5 @@ class MainActivity : AppCompatActivity() {
                 .into(imageView)
         }
 
-    }
+    }*/
 }
---
+++ app/src/main/java/com/example/randomspaceimage/SpaceAdapter.kt
@@ -0,0 +1,78 @@
+package com.example.randomspaceimage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class SpaceAdapter(private val spaceList: MutableList<Triple<String, String, String>>): RecyclerView.Adapter<SpaceAdapter.ViewHolder>() {
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val spaceImage: ImageView
+        val spaceTitle: TextView
+        val spaceDate: TextView
+
+
+        init {
+            // Find our RecyclerView item's ImageView for future use
+            spaceImage = view.findViewById(R.id.space_image)
+            spaceTitle = view.findViewById(R.id.spaceText1)
+            spaceDate = view.findViewById(R.id.spaceText2)
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.space_item, parent, false)
+
+        return ViewHolder(view)
+
+
+    }
+
+
+    override fun getItemCount() = spaceList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.spaceTitle.text = spaceList[position].second
+        holder.spaceDate.text = spaceList[position].third
+
+        Glide.with(holder.itemView)
+            .load(spaceList[position].first)
+            .centerCrop()
+            .into(holder.spaceImage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
